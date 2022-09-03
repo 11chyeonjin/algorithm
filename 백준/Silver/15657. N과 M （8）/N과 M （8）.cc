9eddc97d47b916260853e8c5ff78f5c768cdd32d
11chyeonjin <104848041+11chyeonjin@users.noreply.github.com>
2022-09-03 20:00:51 +0900

    [Silver III] Title: N과 M (8), Time: 8 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+
+bool compare(int a, int b) {
+  return a < b;
+}
+
+int library[9];
+// int visited[9];
+
+int user[9];
+
+void dfs(int count, int now)
+{
+  if (count == M) {
+    for (int i = 0; i < M; i++) {
+      cout << user[i] << " ";
+    }
+    cout << "\n";
+  }
+  else {
+    for (int i = now; i < N; i++) {
+      // if (!visited[i]) {
+        user[count] = library[i];
+        // visited[i] = true;
+        dfs(count + 1, i);
+        // visited[i] = false;
+      }
+    }
+}
+
+int main() {
+  cin >> N >> M;
+  int in;
+  for (int i = 0; i < N; i++) {
+    cin >> in;
+    library[i] = in;
+  }
+  sort(library, library + N, compare);
+  
+  dfs(0, 0);
+  
+  return 0;
+}
