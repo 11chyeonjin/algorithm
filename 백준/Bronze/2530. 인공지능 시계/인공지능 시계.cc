9f569063600784b1fb8735a48230bd75b34ea37a
11chyeonjin <104848041+11chyeonjin@users.noreply.github.com>
2022-09-25 15:14:52 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, C;
+    cin >> A >> B >> C;
+    int D;
+    cin >> D;
+    C += D;
+    B += C / 60;
+    C %= 60;
+    A += B / 60;
+    B %= 60;
+    A %= 24;
+    cout << A << " " << B << " " << C;
+    return 0;
+}
