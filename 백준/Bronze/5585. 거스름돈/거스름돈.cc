332fd0d3d915dd24848d78bf76d0bbfabd751429
hyeonjin <104848041+11chyeonjin@users.noreply.github.com>
2023-04-10 19:24:47 +0900

    [Bronze II] Title: 거스름돈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.cc
+++ 백준/Bronze/5585. 거스름돈/거스름돈.cc
@@ -1,30 +1,21 @@
 #include <iostream>
 using namespace std;
 
+int N;
+int coin[6] = { 500, 100, 50, 10, 5, 1 };
+
 int main() {
-  int user;
-  cin >> user;
-  user = 1000 - user;
-  
-  int result = 0;
-  result += user / 500;
-  user %= 500;
-
-  result += user / 100;
-  user %= 100;
-
-  result += user / 50;
-  user %= 50;
-
-  result += user / 10;
-  user %= 10;
-
-  result += user / 5;
-  user %= 5;
-
-  result += user;
-
-  cout << result;
-
-  return 0;
+    cin >> N;
+    
+    int result = 0;
+    
+    N = 1000 - N;
+    for (int i = 0; i < 6; i++) {
+        result += N / coin[i];
+        N %= coin[i];
+    }
+    
+    cout << result;
+    
+    return 0;
 }
