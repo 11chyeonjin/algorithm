a72c6eb15e5137bdc0cdc36b7626eb080c895270
hyeonjin <104848041+11chyeonjin@users.noreply.github.com>
2023-12-17 04:56:01 +0900

    [level 3] Title: 섬 연결하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -1,57 +1,50 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
-#define INF 987654321
+#include <queue>
+
+#define INF 987654321;
 using namespace std;
 
-int dist[101] = { 0, };
-bool selected[101] = { false };
+struct comp {
+    bool operator()(pair<int,int> p1, pair<int,int> p2) {
+        return p1.first > p2.first;
+    }
+};
 
-int solution(int n, vector<vector<int>> costs) {
-    vector<pair<int,int>> v[n];
+bool visited[101] = { false, };
+
+int solution(int n, vector<vector<int>> costs)  {
+    vector<pair<int, int>> v[n];
     for (int i = 0; i < costs.size(); i++) {
         v[costs[i][0]].push_back({costs[i][1], costs[i][2]});
-        v[costs[i][1]].push_back({costs[i][0], costs[i][2]}); 
+        v[costs[i][1]].push_back({costs[i][0], costs[i][2]});
     }
-    
+
     int answer = 0;
-    fill(dist, dist + n, INF);
     
-    dist[0] = 0;
-    
-    selected[0] = true;
+    priority_queue<pair<int, int>, vector<pair<int,int>>, comp> pq; // dist, next_index
+    visited[0] = true;
     for (int i = 0; i < v[0].size(); i++) {
-        dist[v[0][i].first] = v[0][i].second;
+        pq.push({v[0][i].second, v[0][i].first}); // dist, next_index
     }
     
-    for (int i = 0; i < n - 1; i++) { // E = V - 1 = N - 1 => 0 ~ n-2
-        int min_cost = INF;
-        int min_index = -1;
+    while(!pq.empty()) {
+        int dist = pq.top().first;
+        int idx = pq.top().second;
+        pq.pop();
         
-        for (int j = 0; j < n; j++) {
-            if (selected[j])
-                continue;
+        if (!visited[idx]) {
+            visited[idx] = true;
+            answer += dist;
             
-            if (min_cost > dist[j]) {
-                min_cost = dist[j];
-                min_index = j;
+            for (int i = 0; i < v[idx].size(); i++) {
+                if (!visited[v[idx][i].first])
+                    pq.push({v[idx][i].second, v[idx][i].first});
             }
         }
-        selected[min_index] = true;
-        cout << min_cost << "\n";
-        answer += min_cost;
-        
-        for (int j = 0; j < v[min_index].size(); j++) {
-            int next = v[min_index][j].first;
-            int next_dist = v[min_index][j].second;
-            
-            if (selected[next])
-                continue;
-            
-            dist[next] = min(dist[next], next_dist);
-        }
     }
+    
     
     return answer;
 }
