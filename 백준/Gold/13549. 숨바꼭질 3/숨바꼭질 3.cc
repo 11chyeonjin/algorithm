e665a810f7cd6021454b4a2ed4ee1f1db5aa7748
hyeonjin <104848041+11chyeonjin@users.noreply.github.com>
2024-03-10 18:59:30 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 8 ms, Memory: 2808 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -1,63 +1,75 @@
 #include <iostream>
+#include <vector>
 #include <queue>
-#include <algorithm>
+#define INF 987654321
 using namespace std;
 
 int N, K;
-int visited[100001];
-queue<int> q;
 
-void tpVisited(int a, int t) { // 방문하지 않은 정점 a와 텔포 정점에 대해서
-  int tmp = a;  // 모두 방문처리(도달시간 확인)와 큐에 push를 진행함
-  while(0 < tmp && tmp <= 100000) {
-    if (visited[K] != -1)
-      break;
-    if (visited[tmp] == -1) {
-      visited[tmp] = t;
-      q.push(tmp);
-    }
-    tmp *= 2;
+struct comp {
+  bool operator()(pair<int,int> p1, pair<int,int> p2) {
+    return p1.first > p2.first;
   }
-}
+};
 
-void bfs() {
-  q.push(N);
-  visited[N] = 0;
-  while(visited[K] == -1 && !q.empty()) {
-    int now = q.front();
-    q.pop();
-    tpVisited(now, visited[now]);
-    if (visited[K] != -1)
-      break;
+int bfs(vector<int>& time_v) { 
+  // 찾자마자 즉시 종료하기 위해 pq 사용
+  priority_queue<pair<int,int>, vector<pair<int,int>>, comp> pq; 
+  // pair의 형태는 {cur_time, cur_node}
+  pq.push({0, N});
+  time_v[N] = 0;
+
+  // 최대 탐색 기준
+  int maximum;
+  if (N < K) {
+    maximum = min(100001, 2 * K + 1);
+  } else {
+    maximum = N + 1;
+  }
+
+  while (!pq.empty()) {
+    int cur = pq.top().second;
+    int cur_time = pq.top().first;
+    pq.pop();
+
+    // +1 로 걸을 경우
+    if (0 <= cur + 1  && cur + 1 < maximum) {
+      if (time_v[cur + 1] > cur_time + 1) {
+        pq.push({cur_time + 1, cur + 1});
+        time_v[cur + 1] = cur_time + 1;
+      }
+    }
     
-    int next;
-    
-    next = now + 1;
-    if (0 <= next && next <= 100000) {
-      if (visited[next] == -1) {
-        q.push(next);
-        visited[next] = visited[now] + 1;
+    // -1 로 걸을 경우
+    if (0 <= cur - 1  && cur - 1 < maximum) {
+      if (time_v[cur - 1] > cur_time + 1) {
+        pq.push({cur_time + 1, cur - 1});
+        time_v[cur - 1] = cur_time + 1;
       }
     }
 
-    next = now - 1;
-    if (0 <= next && next <= 100000) {
-      if (visited[next] == -1) {
-        q.push(next);
-        visited[next] = visited[now] + 1;
-      }
+    // 순간 이동의 경우
+    while(0 < cur * 2 && cur * 2 < maximum) {
+      if (time_v[cur * 2] > cur_time) {
+        pq.push({cur_time, cur * 2});
+        time_v[cur * 2] = cur_time;
+      } 
+      // else {
+      //   break;
+      // }
+      cur *= 2; // 논리상 없어도 될 지도..? 어떻게든 한번씩 다 순간이동했을 것
     }
   }
+
+  return time_v[K];
 }
 
 int main() {
   cin >> N >> K;
 
-  fill(visited, visited + 100001, -1);
+  vector<int> time_v(100001, INF);
 
-  bfs();
-
-  cout << visited[K];
+  cout << bfs(time_v);
 
   return 0;
 }
