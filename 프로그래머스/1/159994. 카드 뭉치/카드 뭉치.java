ee6dee41baf02cc8eb893dfa7423a86f9307dcc9
hyeonjin <104848041+11chyeonjin@users.noreply.github.com>
2026-08-12 19:08:26 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        int card1Idx = 0;
+        int card2Idx = 0;
+        
+        for (int i = 0; i <= goal.length; i++) {
+            if (i == goal.length) {
+                return "Yes";
+            }
+            
+            if (card1Idx < cards1.length && goal[i].equals(cards1[card1Idx])) {
+                card1Idx++;
+                continue;
+            }
+            
+            if (card2Idx < cards2.length && goal[i].equals(cards2[card2Idx])) {
+                card2Idx++;
+                continue;
+            }
+            
+            return "No";
+        }
+        
+        return "No";
+    }
+}
